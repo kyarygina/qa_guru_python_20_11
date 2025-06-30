ca917d1ae1a59534bdd5f4213946c6b125bbe8ba
Ksenia Yarygina <ks.yarygina@yandex.ru>
2025-06-30 19:44:29 +0400

    Задание 11. Jenkins. Создаем первую задачу.

--- conftest.py
+++ conftest.py
@@ -1,30 +1,33 @@
 import pytest
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
+from selene.support.shared import browser
 
 from utils import attach
 
-@pytest.fixture(scope='function')
-def setup_browser(request):
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": "128.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
-    browser = Browser(Config(driver))
-    yield browser
+    browser.config.driver = driver
+    browser.config.base_url = "https://demoqa.com"
+    browser.config.timeout = 10
+
+    yield
 
     attach.add_screenshot(browser)
     attach.add_logs(browser)
